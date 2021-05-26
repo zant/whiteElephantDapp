7cfa0daa1e83089f7635abb8be14bbe31aa9981e
gonza <gonzalo.christobal@gmail.com>
2021-05-26 17:21:37 -0400

    Add logo and button centered

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,16 +5,26 @@ import { Container, ContentBlock, ContentRow, MainContent, Section, SectionRow }
 import { Label } from '../typography/Label'
 import { TextInline } from '../typography/Text'
 import { Title } from '../typography/Title'
+import whiteElephantLogo from '../assets/images/white-elephant133x100.svg'
+import { Button } from '../components/base/Button'
+import { useHistory } from 'react-router'
 
 export function Home() {
+  const history = useHistory()
+
   return (
     <MainContentHome>
       <Container>
         <Section>
-          <SectionRow>
-            <Title>White Elephant exchange</Title>
+          <SectionRow justifyContent="center">
+            <img src={whiteElephantLogo} width="300" alt="White Elephant Logo" />
           </SectionRow>
-          <ContentBlock></ContentBlock>
+          <SectionRow justifyContent="center">
+            <Title>White Elephant Exchange</Title>
+          </SectionRow>
+          <SectionRow justifyContent="center">
+            <Button onClick={() => history.push('/play')}>Play</Button>
+          </SectionRow>
         </Section>
       </Container>
     </MainContentHome>
